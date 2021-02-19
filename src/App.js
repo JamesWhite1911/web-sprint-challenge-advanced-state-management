367eccd8914d44596e8bd7d9b5010d76a4d50507
James <JamesWhite1911@protonmail.com>
2021-02-19 17:56:48 -0500

    fixed error testid issue?

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App" data-testid="errorAlert">
         <Header />
         <main>
           <SmurfList fetchSmurfs={fetchSmurfs()} />
